66607f4ce349e0de202148576f0cb35d88793f13
Pranesh <pranesh.vimal5@gmail.com>
2021-12-04 13:40:55 +0530

    Role set to the users in UserServiceImpl

--- src/main/java/com/service/UserServiceImpl.java
+++ src/main/java/com/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.model.Role;
 import com.model.User;
 import com.repository.RoleRepository;
 import com.repository.UserRepository;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,7 +24,10 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void save(User user) {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-		user.setRoles(new HashSet<>(roleRepository.findAll()));
+		Set<Role> roles = new HashSet<>();
+		Role userRole = roleRepository.findByName("STAFF");
+		roles.add(userRole);
+		user.setRoles(roles);
 		userRepository.save(user);
 	}
 
