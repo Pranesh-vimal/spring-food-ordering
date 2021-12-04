0edc87c9985e75d915619095b95b5c9a02594405
Pranesh <pranesh.vimal5@gmail.com>
2021-12-04 23:38:25 +0530

    ProductServiceImpl added

---
+++ src/main/java/com/service/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.model.Product;
+import com.repository.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public Product findByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
