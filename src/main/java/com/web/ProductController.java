480d3eef00c747af2a58a24deb4dd1315766b362
Pranesh <pranesh.vimal5@gmail.com>
2021-12-04 23:39:06 +0530

    ProductController method added

--- src/main/java/com/web/ProductController.java
+++ src/main/java/com/web/ProductController.java
@@ -1,15 +1,39 @@
 package com.web;
 
-import com.WebSecurityConfig;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
+import com.WebSecurityConfig;
+import com.model.Product;
+import com.service.ProductService;
+import com.validator.ProductValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductValidator productValidator;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
@@ -17,5 +41,53 @@ public class ProductController {
             return "redirect:/admin/dashboard";
         }
         return "index";
+    }
+
+    @GetMapping("/admin/products")
+    public String adminProducts(Model model) {
+        model.addAttribute("products", productService.findAll());
+
+        return "admin/productsList";
+    }
+
+    @GetMapping("/admin/products/create")
+    public String adminProductsCreate(Model model) {
+        model.addAttribute("productForm", new Product());
+        return "admin/productsForm";
+    }
+
+    @PostMapping("/admin/products/create")
+    public String adminProductsForm(@ModelAttribute("productForm") Product productForm, BindingResult bindingResult,
+            @RequestParam("image") MultipartFile file) throws IOException {
+        productValidator.validate(productForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "admin/productsForm";
+        }
+
+        String projectDir = Paths.get("").toAbsolutePath().toString();
+        String fileImgname = StringUtils.cleanPath(file.getOriginalFilename());
+
+        String uploadImgDir = projectDir + "\\src\\main\\resources\\static\\images";
+
+        Path uploadImgpath = Paths.get(uploadImgDir);
+
+        if (!Files.exists(uploadImgpath)) {
+            Files.createDirectories(uploadImgpath);
+        }
+
+        try {
+            InputStream is = file.getInputStream();
+            Path fileImgpath = uploadImgpath.resolve(fileImgname);
+            Files.copy(is, fileImgpath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        productForm.setImageUrl("\\images\\" + fileImgname);
+
+        productService.save(productForm);
+
+        return "redirect:/admin/products";
     }
 }
