1240f95cec955369685e01ffe18b7f6bd3be3544
Pranesh <pranesh.vimal5@gmail.com>
2021-12-04 12:52:09 +0530

    @RequestMapping("/admin") added in UserController

--- src/main/java/com/web/UserController.java
+++ src/main/java/com/web/UserController.java
@@ -1,6 +1,9 @@
 package com.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +15,7 @@ import com.service.UserService;
 import com.validator.UserValidator;
 
 @Controller
+@RequestMapping("/admin")
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -24,6 +28,9 @@ public class UserController {
 
 	@GetMapping("/registration")
 	public String registration(Model model) {
+		if (isAuthenticated()) {
+			return "redirect:/admin/welcome";
+		}
 		model.addAttribute("userForm", new User());
 
 		return "registration";
@@ -41,11 +48,14 @@ public class UserController {
 
 		securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
 
-		return "redirect:/welcome";
+		return "redirect:/admin/welcome";
 	}
 
 	@GetMapping("/login")
 	public String login(Model model, String error, String logout) {
+		if (isAuthenticated()) {
+			return "redirect:/admin/welcome";
+		}
 		if (error != null)
 			model.addAttribute("error", "Your username and password is invalid.");
 
@@ -55,8 +65,16 @@ public class UserController {
 		return "login";
 	}
 
-	@GetMapping({ "/", "/welcome" })
+	@GetMapping({ "/welcome" })
 	public String welcome(Model model) {
 		return "welcome";
+	}
+
+	private boolean isAuthenticated() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
+			return false;
+		}
+		return authentication.isAuthenticated();
 	}
 }
