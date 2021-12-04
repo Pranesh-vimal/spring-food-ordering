8e61e364c708aef8ce47500c70dd4e1e6fd274e2
Pranesh <pranesh.vimal5@gmail.com>
2021-12-04 18:28:19 +0530

    isAuthenticated() added in WebSecurityConfig

--- src/main/java/com/WebSecurityConfig.java
+++ src/main/java/com/WebSecurityConfig.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -26,7 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/resources/**", "/admin/registration").permitAll().anyRequest().authenticated()
+		http.authorizeRequests()
+				.antMatchers("/admin/dashboard").authenticated()
+				.antMatchers("/resources/**", "/admin/registration", "/**").permitAll().anyRequest()
+				.anonymous()
 				.and().formLogin().loginPage("/admin/login").permitAll().and().logout().permitAll();
 	}
 
@@ -35,8 +41,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return authenticationManager();
 	}
 
-//	@Autowired
-//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-//	}
+	// @Autowired
+	// public void configureGlobal(AuthenticationManagerBuilder auth) throws
+	// Exception {
+	// auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	// }
+
+	public static boolean isAuthenticated() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
+			return false;
+		}
+		return authentication.isAuthenticated();
+	}
 }
