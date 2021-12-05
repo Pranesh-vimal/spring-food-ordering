85e612bdc874f97f1c131bf4b560cf59cfc7d6df
Pranesh <pranesh.vimal5@gmail.com>
2021-12-05 23:59:56 +0530

    cart removed

--- src/main/java/com/model/Cart.java
+++ src/main/java/com/model/Cart.java
@@ -14,49 +14,4 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-    @OneToOne
-    private User user;
-
-    @OneToOne
-    private Product product;
-
-    private int quantity;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    @Override
-    public String toString() {
-        return "Cart [id=" + id + ", product=" + product + ", quantity=" + quantity + ", user=" + user + "]";
-    }
 }
