10740dd67b09877c419aa45a60578753db58d3bc
Pranesh <pranesh.vimal5@gmail.com>
2021-12-06 14:28:03 +0530

    CartItemServiceImpl added

---
+++ src/main/java/com/service/CartItemServiceImpl.java
@@ -0,0 +1,52 @@
+package com.service;
+
+import java.util.Set;
+
+import com.model.Cart;
+import com.model.CartItem;
+import com.model.Product;
+import com.repository.CartItemRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartItemServiceImpl implements CartItemService {
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Override
+    public void addToCart(Cart cart, Product product) {
+        CartItem cartItem = cartItemRepository.findByCartIdAndProductId(cart.getId(), product.getId());
+
+        if (cartItem == null) {
+            cartItem = new CartItem();
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setSubTotal(cartItem.getProduct().getPrice());
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+            cartItem.setSubTotal(cartItem.getSubTotal() + cartItem.getProduct().getPrice());
+        }
+
+        cartItemRepository.save(cartItem);
+    }
+
+    @Override
+    public double getTotalPrice(Cart cart) {
+
+        double totalPrice = 0;
+        for (CartItem cartItem : cart.getCartItems()) {
+            totalPrice += cartItem.getSubTotal();
+        }
+
+        return totalPrice;
+    }
+
+    @Override
+    public Set<CartItem> findByCartId(int id) {
+        return cartItemRepository.findByCartId(id);
+    }
+}
