ef7a2c9423e8e861e2556eb27a6cc1764348c608
Pranesh <pranesh.vimal5@gmail.com>
2021-12-06 14:26:28 +0530

    Cart fields added

--- src/main/java/com/model/Cart.java
+++ src/main/java/com/model/Cart.java
@@ -1,9 +1,12 @@
 package com.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,4 +16,49 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String session;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItem> cartItems;
+
+    private double totalPrice;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+
+    public Set<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(Set<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart [cartItems=" + cartItems + ", id=" + id + ", session=" + session + ", totalPrice=" + totalPrice
+                + "]";
+    }
 }
