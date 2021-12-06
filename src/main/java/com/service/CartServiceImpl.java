cc4d64166ef080564d628aa6a8c0550c8d1c9edb
Pranesh <pranesh.vimal5@gmail.com>
2021-12-06 14:28:42 +0530

    CartServiceImpl added

--- src/main/java/com/service/CartServiceImpl.java
+++ src/main/java/com/service/CartServiceImpl.java
@@ -1,8 +1,41 @@
 package com.service;
 
+import java.util.Set;
+
+import com.model.Cart;
+import com.model.CartItem;
+import com.model.Product;
+import com.repository.CartRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartServiceImpl implements CartService {
 
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Override
+    public void addToCart(String session, Product product) {
+        Cart cart = cartRepository.findBySession(session);
+        if (cart == null) {
+            cart = new Cart();
+            cart.setSession(session);
+            cartRepository.save(cart);
+        }
+
+        cartItemService.addToCart(cart, product);
+
+        Set<CartItem> cartItems = cartItemService.findByCartId(cart.getId());
+
+        cart.setCartItems(cartItems);
+
+        cart.setTotalPrice(cartItemService.getTotalPrice(cart));
+
+        cartRepository.save(cart);
+    }
 }
