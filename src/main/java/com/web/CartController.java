40734f1abdcd6845ca3d53f783520dcf91941db9
Pranesh <pranesh.vimal5@gmail.com>
2021-12-07 19:59:40 +0530

    checkout() added in CartController

--- src/main/java/com/web/CartController.java
+++ src/main/java/com/web/CartController.java
@@ -1,14 +1,18 @@
 package com.web;
 
 import com.model.Cart;
+import com.model.Order;
 import com.model.Product;
 import com.service.CartService;
+import com.service.OrderService;
 import com.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 
@@ -21,6 +25,9 @@ public class CartController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private OrderService orderService;
 
     @GetMapping("/add/{id}")
     public Cart addToCart(@PathVariable("id") int id) {
@@ -72,6 +79,23 @@ public class CartController {
             cartService.deleteCart(session, product);
             Cart cart = cartService.findBySession(session);
             return cart;
+        }
+        return null;
+    }
+
+    @PostMapping("/checkout")
+    public String checkout(@RequestParam("name") String name, @RequestParam("email") String email,
+            @RequestParam("phone") String phone) {
+        Order order = new Order();
+        order.setName(name);
+        order.setEmail(email);
+        order.setPhone(phone);
+        String session = RequestContextHolder.currentRequestAttributes().getSessionId();
+        Cart cart = cartService.findBySession(session);
+        if (cart != null) {
+            orderService.checkout(cart, order);
+            cartService.clearCart(session);
+            return "redirect:/success";
         }
         return null;
     }
