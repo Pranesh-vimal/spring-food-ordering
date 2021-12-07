8992c7bc99562243db0e407eb40d2f5246425726
Saravana Raja <saravanaraja25@gmail.com>
2021-12-07 11:50:44 +0530

    increase and decrease quantity cart js

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,5 +1,6 @@
 $('document').ready(function(){
     // console.log("Hello World");
+    getCart();
 });
 
 async function addtoCart(id){
@@ -21,9 +22,9 @@ async function addtoCart(id){
                             <h6 class="card-title">${item.product.name}</h6>
                             <p class="mb-2 card-text">Rs. ${item.product.price} </p>
                             <div class="d-inline">
-                                <button class="btn w-20 fw-bold text-center btn-dark">-</button>
+                                <button onclick="removeFromCart(${item.product.id})" class="btn w-20 fw-bold text-center btn-dark">-</button>
                                 <p  class="w-20 d-inline text-center fs-6 py-2 rounded px-3 border cart-quantity" >${item.quantity}</p>
-                                <button class="btn w-20 btn-dark">+</button>
+                                <button onclick="addtoCart(${item.product.id})" class="btn w-20 btn-dark">+</button>
                                 <button class="btn float-end btn-danger w-20 pe-4 text-center"><i class="far fa-trash-alt"></i></button>
                             </div>
                             <p class="mb-2 card-text fw-bold">Rs. ${item.product.price}</p>
@@ -35,3 +36,51 @@ async function addtoCart(id){
     });
 
 }
+
+async function getCart(){
+    $('.cart-item').empty();
+    $('.cart-body').addClass('d-none');
+    const res=await axios.get('/cart');
+    const data=res.data;
+    // console.log(data=="");
+    if(data!="" && data.cartItems.length>0){
+        $('.no-items').addClass('d-none');
+        $('.cart-body').removeClass('d-none');
+        data.cartItems.forEach(function(item){
+            $('.cart-item').append(`
+                <div class="item mb-3">                                        
+                    <div class="card d-flex flex-row" style="height: 10rem;">
+                        <div class="card-body d-flex">
+                            <img src="${item.product.imageUrl}" style="width:75px; height: 100%" class=" card-img-top" alt="${item.product.name}">
+                            <div class="content px-2">
+                                <h6 class="card-title">${item.product.name}</h6>
+                                <p class="mb-2 card-text">Rs. ${item.product.price} </p>
+                                <div class="d-inline">
+                                    <button onclick="removeFromCart(${item.product.id})" class="btn w-20 fw-bold text-center btn-dark">-</button>
+                                    <p  class="w-20 d-inline text-center fs-6 py-2 rounded px-3 border cart-quantity" >${item.quantity}</p>
+                                    <button onclick="addtoCart(${item.product.id})" class="btn w-20 btn-dark">+</button>
+                                    <button class="btn float-end btn-danger w-20 pe-4 text-center"><i class="far fa-trash-alt"></i></button>
+                                </div>
+                                <p class="mb-2 card-text fw-bold">Rs. ${item.product.price}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `);
+        });
+    }
+    else{
+        $('.no-items').removeClass('d-none');
+        $('.cart-body').addClass('d-none');
+    }
+}
+
+async function removeFromCart(id){
+    const res=await axios.get(`/cart/remove/${id}`);
+    getCart();
+}
+
+// async function deleteItem(id){
+//     const res=await axios.get(`/cart/delete/${id}`);
+//     getCart();
+// }
