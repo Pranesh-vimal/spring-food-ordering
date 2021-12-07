149a4f8f145b449e7cd0bd85bb00fe3a7ba15b75
Pranesh <pranesh.vimal5@gmail.com>
2021-12-07 23:32:53 +0530

    OrderController unwanted import removed

--- src/main/java/com/web/OrderController.java
+++ src/main/java/com/web/OrderController.java
@@ -1,26 +1,13 @@
 package com.web;
 
-import com.model.Order;
-import com.service.OrderService;
-import com.service.ProductService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class OrderController {
-
-    @Autowired
-    private OrderService orderService;
-
-    @Autowired
-    private ProductService productService;
 
     @GetMapping("/orders")
     public String orderSearchForm(Model model) {
