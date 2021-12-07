09836e174bf4b40e85a7c1ce1ed59a47ad931b7b
Pranesh <pranesh.vimal5@gmail.com>
2021-12-07 19:59:04 +0530

    checkout() logic added in OrderServiceImpl

--- src/main/java/com/service/OrderServiceImpl.java
+++ src/main/java/com/service/OrderServiceImpl.java
@@ -1,6 +1,11 @@
 package com.service;
 
+import java.sql.Date;
+import java.util.Set;
+
 import com.model.Cart;
+import com.model.CartItem;
+import com.model.Order;
 import com.repository.OrderRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +21,17 @@ public class OrderServiceImpl implements OrderService {
     private OrderItemService orderItemService;
 
     @Override
-    public void checkout(Cart cart) {
-        
+    public void checkout(Cart cart, Order order) {
+        order.setTotal(cart.getTotalPrice());
+        order.setDate(new Date(System.currentTimeMillis()));
+        order.setStatus("Created");
+        Set<CartItem> cartItems = cart.getCartItems();
+        for (CartItem cartItem : cartItems) {
+            orderItemService.createOrderItem(cartItem, order);
+        }
+
+        order.setOrderItems(orderItemService.findByOrder(order));
+        orderRepository.save(order);
     }
 
 }
