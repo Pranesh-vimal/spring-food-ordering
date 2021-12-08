cfe6457e5b61f3bcc40a6b662f65924d52619f2e
Pranesh <pranesh.vimal5@gmail.com>
2021-12-08 10:05:34 +0530

    getOrder() added inOrderController

--- src/main/java/com/web/OrderController.java
+++ src/main/java/com/web/OrderController.java
@@ -1,13 +1,24 @@
 package com.web;
 
+import java.util.Map;
+
+import com.model.Order;
+import com.service.OrderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
 
     @GetMapping("/orders")
     public String orderSearchForm(Model model) {
@@ -15,5 +26,16 @@ public class OrderController {
         return "OrderSearch";
     }
 
-    
+    @PostMapping("/orders")
+    public String getOrder(Model model, @RequestParam Map<String, String> requestParams) {
+
+        int orderId = Integer.parseInt(requestParams.get("orderId"));
+        String email = requestParams.get("email").toString();
+        Order order = orderService.getOrder(orderId, email);
+        System.out.println(order);
+        model.addAttribute("order", order);
+        model.addAttribute("title", "Order Details");
+        return "OrderDetails";
+    }
+
 }
