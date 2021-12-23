15bd13d91e5e4e4505346b2c64104a761daf9e6b
Pranesh <pranesh.vimal5@gmail.com>
2021-12-23 19:23:58 +0530

    findAllByOrderByIdDesc() added in OrderRepository

--- src/main/java/com/repository/OrderRepository.java
+++ src/main/java/com/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.repository;
 
+import java.util.List;
+
 import com.model.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findByEmail(String email);
 
     Order findById(int orderId);
+
+    List<Order> findAllByOrderByIdDesc();
 
 }
